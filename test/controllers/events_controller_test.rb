b106771ce69c89284135e2018bc5afac851fc5e9
Olivia Bonnal <oliviabonnal@gmail.com>
2023-06-28 10:46:28 +0100

    Fix sign in error for events#edit and #update test
    In the EventsController, only #edit and #update require the user to be
    signed in, so I've added sign_in_as @user on these tests.

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class EventsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @event = events(:one)
+    @user = users(:lazaro_nixon)
   end
 
   test "should get index" do
@@ -16,11 +17,13 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    sign_in_as @user
     get edit_event_url(@event)
     assert_response :success
   end
 
   test "should update event" do
+    sign_in_as @user
     patch event_url(@event), params: {event: {description: @event.description, frequency: @event.frequency, kind: @event.kind, name: @event.name, website: @event.website}}
     assert_redirected_to event_url(@event)
   end
