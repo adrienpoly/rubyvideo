1b98e2d17d9fd945c9a7c5f84f71495db1b96d31
Adrien POLY <adrienpoly@gmail.com>
2023-06-29 13:12:33 +0200

    configure capybara and add wait_for_trubo

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,4 +1,18 @@
 require "test_helper"
+require "webdrivers"
+
+Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless") unless ActiveModel::Type::Boolean.new.cast(ENV["HEADFUL"])
+  options.add_argument("--window-size=1920,1080")
+
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 240
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options], http_client: client)
+end
+
+Capybara.javascript_driver = :headless_chrome
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
@@ -12,4 +26,12 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     assert_current_path root_url
     user
   end
+
+  def wait_for_turbo(timeout = 2)
+    if has_css?(".turbo-progress-bar", visible: true, wait: 0.25.seconds)
+      has_no_css?(".turbo-progress-bar", wait: timeout)
+    end
+  end
 end
+
+Capybara.default_max_wait_time = 5 # Set the wait time in seconds
