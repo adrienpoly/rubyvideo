35bc334b18f5f3c1e1f182545a6d1252b8f926a8
Adrien POLY <adrienpoly@gmail.com>
2023-07-21 07:21:15 +0200

    disable meillisearch during seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,53 +34,58 @@ speakers.each do |speaker|
   end
 end
 
-organisations.each do |organisation|
-  organisation = Organisation.find_or_create_by!(name: organisation["name"]) do |org|
-    org.website = organisation["website"]
-    # org.twitter = organisation["twitter"]
-    org.youtube_channel_name = organisation["youtube_channel_name"]
-    org.kind = organisation["kind"]
-    org.frequency = organisation["frequency"]
-    org.youtube_channel_id = organisation["youtube_channel_id"]
-    # org.language = organisation["language"]
-  end
-
-  events = YAML.load_file("#{Rails.root}/data/#{organisation.slug}/playlists.yml")
-
-  events.each do |event|
-    event = Event.find_or_create_by!(name: event["title"]) do |evt|
-      evt.date = event["published_at"]
-      evt.organisation = organisation
+MeiliSearch::Rails.deactivate! do
+  organisations.each do |organisation|
+    organisation = Organisation.find_or_create_by!(name: organisation["name"]) do |org|
+      org.website = organisation["website"]
+      # org.twitter = organisation["twitter"]
+      org.youtube_channel_name = organisation["youtube_channel_name"]
+      org.kind = organisation["kind"]
+      org.frequency = organisation["frequency"]
+      org.youtube_channel_id = organisation["youtube_channel_id"]
+      # org.language = organisation["language"]
     end
 
-    puts event.slug
-    talks = YAML.load_file("#{Rails.root}/data/#{organisation.slug}/#{event.slug}/videos.yml")
+    events = YAML.load_file("#{Rails.root}/data/#{organisation.slug}/playlists.yml")
 
-    talks.each do |talk_data|
-      next if talk_data["title"].blank? || videos_to_ignore.include?(talk_data["video_id"])
-
-      talk = Talk.find_or_create_by!(title: talk_data["title"], event: event) do |tlk|
-        tlk.description = talk_data["description"]
-        tlk.year = talk_data["year"]
-        tlk.video_id = talk_data["video_id"]
-        tlk.video_provider = :youtube
-        tlk.date = talk_data["published_at"]
-        tlk.thumbnail_xs = talk_data["thumbnail_xs"] || ""
-        tlk.thumbnail_sm = talk_data["thumbnail_sm"] || ""
-        tlk.thumbnail_md = talk_data["thumbnail_md"] || ""
-        tlk.thumbnail_lg = talk_data["thumbnail_lg"] || ""
-        tlk.thumbnail_xl = talk_data["thumbnail_xl"] || ""
-        tlk.slug = talk_data["raw_title"].parameterize
+    events.each do |event|
+      event = Event.find_or_create_by!(name: event["title"]) do |evt|
+        evt.date = event["published_at"]
+        evt.organisation = organisation
       end
 
-      talk_data["speakers"]&.each do |speaker_name|
-        next if speaker_name.blank?
+      puts event.slug
+      talks = YAML.load_file("#{Rails.root}/data/#{organisation.slug}/#{event.slug}/videos.yml")
 
-        speaker = Speaker.find_by(slug: speaker_name.parameterize) || Speaker.find_or_create_by(name: speaker_name.strip)
-        SpeakerTalk.create(speaker: speaker, talk: talk) if speaker
+      talks.each do |talk_data|
+        next if talk_data["title"].blank? || videos_to_ignore.include?(talk_data["video_id"])
+
+        talk = Talk.find_or_create_by!(title: talk_data["title"], event: event) do |tlk|
+          tlk.description = talk_data["description"]
+          tlk.year = talk_data["year"]
+          tlk.video_id = talk_data["video_id"]
+          tlk.video_provider = :youtube
+          tlk.date = talk_data["published_at"]
+          tlk.thumbnail_xs = talk_data["thumbnail_xs"] || ""
+          tlk.thumbnail_sm = talk_data["thumbnail_sm"] || ""
+          tlk.thumbnail_md = talk_data["thumbnail_md"] || ""
+          tlk.thumbnail_lg = talk_data["thumbnail_lg"] || ""
+          tlk.thumbnail_xl = talk_data["thumbnail_xl"] || ""
+          tlk.slug = talk_data["raw_title"].parameterize
+        end
+
+        talk_data["speakers"]&.each do |speaker_name|
+          next if speaker_name.blank?
+
+          speaker = Speaker.find_by(slug: speaker_name.parameterize) || Speaker.find_or_create_by(name: speaker_name.strip)
+          SpeakerTalk.create(speaker: speaker, talk: talk) if speaker
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        puts "#{talk.title} is duplicated #{e.message}"
       end
-    rescue ActiveRecord::RecordInvalid => e
-      puts "#{talk.title} is duplicated #{e.message}"
     end
   end
 end
+
+# reindex all talk in MeiliSearch
+Talk.reindex!
