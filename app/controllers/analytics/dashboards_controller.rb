4b8735a678d031f792bc61f85fda22d59185856e
Adrien POLY <adrienpoly@gmail.com>
2023-08-27 01:54:55 +0200

    add analytics page

--- app/controllers/analytics/dashboards_controller.rb
+++ app/controllers/analytics/dashboards_controller.rb
@@ -2,5 +2,7 @@ class Analytics::DashboardsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
+    @daily_visits = Ahoy::Visit.where(started_at: 60.days.ago..).group_by_day(:started_at).count
+    @daily_page_views = Ahoy::Event.where(time: 60.days.ago..).group_by_day(:time).count
   end
 end
