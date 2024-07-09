c3d36d2202c14efa5d45978f5c4eb4ae38779cbf
Adrien POLY <adrienpoly@gmail.com>
2024-07-09 09:08:57 +0200

    return empty transcript when missing

--- app/serializers/transcript_serializer.rb
+++ app/serializers/transcript_serializer.rb
@@ -4,10 +4,10 @@ class TranscriptSerializer
   end
 
   def self.load(transcript_json)
-    return nil if transcript_json.nil? || transcript_json.empty?
+    transcript = Transcript.new
+    return transcript if transcript_json.nil? || transcript_json.empty?
 
     cues_array = JSON.parse(transcript_json, symbolize_names: true)
-    transcript = Transcript.new
     cues_array.each do |cue_hash|
       transcript.add_cue(Cue.new(cue_hash[:start_time], cue_hash[:end_time], cue_hash[:text]))
     end
