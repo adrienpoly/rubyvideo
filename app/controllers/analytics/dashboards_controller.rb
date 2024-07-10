ccbbec9c68b5e52ec2283ff7fec9501b293ed18b
Adrien POLY <adrienpoly@gmail.com>
2024-07-10 06:48:44 +0200

    add monthly traffic

--- app/controllers/analytics/dashboards_controller.rb
+++ app/controllers/analytics/dashboards_controller.rb
@@ -12,4 +12,16 @@ class Analytics::DashboardsController < ApplicationController
       Ahoy::Event.where("date(time) BETWEEN ? AND ?", 60.days.ago.to_date, Date.yesterday).group_by_day(:time).count
     end
   end
+
+  def monthly_visits
+    @monthly_visits = Rails.cache.fetch("monthly_visits", expires_at: Time.current.end_of_day) do
+      Ahoy::Visit.where("date(started_at) BETWEEN ? AND ?", 12.months.ago.to_date.beginning_of_month, Date.yesterday).group_by_month(:started_at).count
+    end
+  end
+
+  def monthly_page_views
+    @monthly_page_views = Rails.cache.fetch("monthly_page_views", expires_at: Time.current.end_of_day) do
+      Ahoy::Event.where("date(time) BETWEEN ? AND ?", 12.months.ago.to_date.beginning_of_month, Date.yesterday).group_by_month(:time).count
+    end
+  end
 end
