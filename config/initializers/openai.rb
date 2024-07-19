95b0a3b5a072a7f7ddef16c550db695796fb816a
Adrien Poly <adrienpoly@gmail.com>
2024-07-19 09:53:00 +0200

    increase timeout

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -2,4 +2,5 @@ OpenAI.configure do |config|
   config.access_token = ENV["OPENAI_ACCESS_TOKEN"]
   config.organization_id = ENV["OPENAI_ORGANIZATION_ID"]
   config.log_errors = Rails.env.development?
+  config.request_timeout = 240
 end
