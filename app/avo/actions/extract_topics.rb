2ac2f1966df1de122202f44ef652ae7763d8332e
Adrien Poly <adrienpoly@gmail.com>
2024-08-15 00:01:13 +0200

    perform_later

--- app/avo/actions/extract_topics.rb
+++ app/avo/actions/extract_topics.rb
@@ -3,8 +3,7 @@ class Avo::Actions::ExtractTopics < Avo::BaseAction
 
   def handle(query:, fields:, current_user:, resource:, **args)
     query.each do |talk|
-      # AnalyzeTalkTopicsJob.perform_later(talk)
-      AnalyzeTalkTopicsJob.new.perform(talk)
+      AnalyzeTalkTopicsJob.perform_later(talk)
     end
   end
 end
