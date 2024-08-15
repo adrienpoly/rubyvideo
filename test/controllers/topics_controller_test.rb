1c545ccab42689f97a4d7bd4e7dc05bcc21ee88d
Marco Roth <marco.roth@intergga.ch>
2024-08-15 13:11:58 +0200

    Fix topic controller tests

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,13 +1,34 @@
 require "test_helper"
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @activesupport = topics(:activesupport)
+
+    @activerecord = topics(:activerecord)
+    @talk = talks(:one)
+    @activerecord.talks << @talk
+  end
+
   test "should get index" do
-    get topics_index_url
+    get topics_url
     assert_response :success
+
+    assert_select "h1", "Topics"
+
+    assert_select "##{dom_id(@activerecord)} > span", "1"
+    assert_select "##{dom_id(@activesupport)} > span", "0"
   end
 
   test "should get show" do
-    get topics_show_url
+    get topic_url(@activerecord)
     assert_response :success
+    assert_select "h1", @activerecord.name
+    assert_select "#topic-talks > div", 1
+    assert_select "##{dom_id(@talk)} h2", @talk.title
+
+    get topic_url(@activesupport)
+    assert_response :success
+    assert_select "h1", @activesupport.name
+    assert_select "#topic-talks > div", 0
   end
 end
