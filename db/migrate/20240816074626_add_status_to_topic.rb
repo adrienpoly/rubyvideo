168d653273c475361f8c124971f4594244383500
Marco Roth <marco.roth@intergga.ch>
2024-08-16 12:43:21 +0200

    Fix published migration

--- db/migrate/20240816074626_add_status_to_topic.rb
+++ db/migrate/20240816074626_add_status_to_topic.rb
@@ -3,6 +3,6 @@ class AddStatusToTopic < ActiveRecord::Migration[7.2]
     add_column :topics, :status, :string, null: false, default: "pending"
     add_index :topics, :status
 
-    Topic.published.update_all(status: "approved")
+    Topic.where(published: true).update_all(status: "approved")
   end
 end
