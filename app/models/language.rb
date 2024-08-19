7577c37fc69c9a14e31a9e2c55cab80ff367ccda
Marco Roth <marco.roth@intergga.ch>
2024-08-19 15:37:13 +0200

    Fallback to `en` if no valid language is found (#142)

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,5 @@
 class Language
+  DEFAULT = "en".freeze
   ALL_ALPHA2_CODES = ISO_639::ISO_639_1.map(&:alpha2).freeze
   ALL_ENGLISH_NAMES = ISO_639::ISO_639_1.map { |language| language.english_name.split(";").first }.freeze
   ALL_LANGUAGES = ALL_ALPHA2_CODES.zip(ALL_ENGLISH_NAMES).to_h.freeze
