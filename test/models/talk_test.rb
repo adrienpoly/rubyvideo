4232a544e4307822903a28ac22f0f883ce20b1df
Adrien Poly <adrienpoly@gmail.com>
2024-08-22 06:35:31 +0200

    default to en when language is nil or invalid (#150)
    * dafult to en when language is nil or invalid
    
    * only default to en when language is nil
    
    * update tests description

--- test/models/talk_test.rb
+++ test/models/talk_test.rb
@@ -142,4 +142,10 @@ class TalkTest < ActiveSupport::TestCase
     assert_equal ["Language can't be blank", "Language  is not a valid IS0-639 alpha2 code"],
       talk.errors.map(&:full_message)
   end
+
+  test "create a new talk with a nil language" do
+    talk = Talk.create!(title: "New title", language: nil)
+    assert_equal "en", talk.language
+    assert talk.valid?
+  end
 end
