fa488fdb6697abc6f85880715332fbec7c643075
Kasper Timm Hansen <hey@kaspth.com>
2024-08-23 20:20:53 +0200

    Start prepping db:seed test fix

--- test/tasks/db_seed_test.rb
+++ test/tasks/db_seed_test.rb
@@ -1,11 +1,20 @@
 require "test_helper"
 
 class DbSeedTest < ActiveSupport::TestCase
+  setup do
+    @old_lookup_paths = Oaken.lookup_paths.dup
+    Oaken.lookup_paths.replace ["db/seeds", "db/seeds/development"]
+  end
+
+  teardown do
+    Oaken.lookup_paths.replace @old_lookup_paths
+  end
+
   test "db:seed runs successfully" do
     skip "TODO: Figure out how to load development seeds"
     Rails.application.load_seed
 
-    # ensure that all talks have a year
+    # ensure that all talks have a date
     assert_empty Talk.where(date: nil).pluck(:title)
 
     # Ensuring idempotency
