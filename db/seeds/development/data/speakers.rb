67cc205b4f1d056001743da43c959a0790feec8a
Kasper Timm Hansen <hey@kaspth.com>
2024-08-23 20:14:25 +0200

    Don't need these

--- db/seeds/development/data/speakers.rb
+++ db/seeds/development/data/speakers.rb
@@ -1,6 +1,4 @@
 speakers = YAML.load_file("#{Rails.root}/data/speakers.yml")
-organisations = YAML.load_file("#{Rails.root}/data/organisations.yml")
-videos_to_ignore = YAML.load_file("#{Rails.root}/data/videos_to_ignore.yml")
 
 # create speakers
 speakers.each do |speaker|
