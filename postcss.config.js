e9e9ad9596f81edf0cc57c3c226d4d8caac950ef
Adrien Poly <adrienpoly@gmail.com>
2024-09-08 23:11:51 +0200

    add rfs fonts

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,15 @@ module.exports = {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
+    rfs: {
+      twoDimensional: false,
+      baseValue: 14,
+      unit: 'rem',
+      breakpoint: 1200,
+      breakpointUnit: 'px',
+      factor: 5,
+      safariIframeResizeBugFix: false
+    },
     autoprefixer: {}
   }
 }
