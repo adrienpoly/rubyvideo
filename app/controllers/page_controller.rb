f351d567bcc38d2d899c32ed55e7ddb8f327db8f
Adrien Poly <adrienpoly@gmail.com>
2024-09-08 14:03:51 +0200

    ensure the speaker has an image

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -13,6 +13,6 @@ class PageController < ApplicationController
     @speakers_count = home_page_cached_data[:speakers_count]
     @latest_talks = Talk.order(date: :desc).limit(4)
     @latest_events = Event.order(date: :desc).limit(4)
-    @active_speakers = Speaker.order(talks_count: :desc).limit(20).sample(4)
+    @active_speakers = Speaker.with_github.order(talks_count: :desc).limit(20).sample(4)
   end
 end
