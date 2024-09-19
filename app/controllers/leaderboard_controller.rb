4c7d9985434e73e04f1a0765ae83f0b11753f67f
Adrien Poly <adrienpoly@gmail.com>
2024-09-19 12:15:06 +0200

    cache leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -6,12 +6,13 @@ class LeaderboardController < ApplicationController
     @ranked_speakers = Speaker.left_joins(:talks)
       .group(:id)
       .order("COUNT(talks.id) DESC")
-      .select("speakers.*, COUNT(talks.id) as talk_count")
+      .select("speakers.*, COUNT(talks.id) as talks_count")
 
     if @filter == "last_12_months"
       @ranked_speakers = @ranked_speakers.where("talks.date >= ?", 12.months.ago.to_date)
     end
-
     @ranked_speakers = @ranked_speakers.limit(100)
+    last_modified_talk = Talk.order(updated_at: :desc).first
+    fresh_when(last_modified: last_modified_talk&.updated_at || Time.current)
   end
 end
