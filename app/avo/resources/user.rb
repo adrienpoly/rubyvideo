42b2dddacd789e07dfa98238dce0c56202d5e10b
Adrien Poly <adrienpoly@gmail.com>
2024-09-19 07:48:00 +0200

    add speaker to user admin resource

--- app/avo/resources/user.rb
+++ app/avo/resources/user.rb
@@ -1,6 +1,6 @@
 class Avo::Resources::User < Avo::BaseResource
   self.title = :name
-  self.includes = []
+  self.includes = [:speaker]
   self.search = {
     query: -> { query.where(email: params[:q]) }
   }
@@ -10,6 +10,7 @@ class Avo::Resources::User < Avo::BaseResource
     field :email, as: :text, link_to_record: true
     field :name, as: :text, link_to_record: true
     field :admin, as: :boolean
+    field :speaker, as: :belongs_to
     field :sessions, as: :has_many
     field :connected_accounts, as: :has_many
   end
