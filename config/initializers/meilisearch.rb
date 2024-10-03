bd1d7668236b134e78bf6b96f161f788493a3854
Adrien Poly <adrienpoly@gmail.com>
2024-10-03 06:46:49 +0200

    update setup with docker compose (#214)
    * update setup with docker compose
    
    * fix ci

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,5 +1,6 @@
 MeiliSearch::Rails.configuration = {
   meilisearch_url: Rails.env.local? ? "http://localhost:7700" : "http://91.107.208.207:7700", # example: http://localhost:7700
-  meilisearch_api_key: ENV["MEILI_MASTER_KEY"],
-  per_environment: true
+  api_key: Rails.env.local? ? nil : ENV.fetch("MEILI_MASTER_KEY"),
+  per_environment: true,
+  timeout: 60
 }
