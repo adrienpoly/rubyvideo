1520d90f08caf423cd7a2413528b1e24e225c92b
Justin Marsh <jmarsh24@gmail.com>
2024-10-18 00:39:25 +0200

    disable parallelized test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,7 +20,7 @@ class ActiveSupport::TestCase
     end
   end
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: 1)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
