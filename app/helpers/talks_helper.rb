c1518b293c7f1bbc28e4ea555e6f1fcb93b6f978
Justin Marsh <jmarsh24@gmail.com>
2024-10-17 18:12:42 +0200

    link_to_youtube

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,21 @@
 module TalksHelper
+  def link_to_talk(talk = nil, options = {}, html_options = {}, &block)
+    raise ArgumentError, "must pass an instance of talk to this helper method" unless talk.present? && talk.is_a?(Talk)
+
+    url, target = if talk.date > 2.weeks.ago
+      ["https://youtube.com/watch?v=#{talk.video_id}", "_blank"]
+    else
+      [talk_path(talk, options), nil]
+    end
+
+    html_options[:target] = target if target
+
+    link_to(url, html_options) do
+      if block
+        yield
+      else
+        talk.to_s
+      end
+    end
+  end
 end
