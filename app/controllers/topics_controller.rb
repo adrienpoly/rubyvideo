a45e1dae739a069b3f28b8380dca027c66b3b51b
Adrien Poly <adrienpoly@gmail.com>
2024-10-17 06:26:27 +0200

    select only required talk columns

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,7 +10,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find_by!(slug: params[:slug])
-    @pagy, @talks = pagy(@topic.talks.order(date: :desc).includes(:speakers, :event), limit: 12, page: params[:page]&.to_i || 1)
+    @pagy, @talks = pagy(@topic.talks.order(date: :desc).select(:id, :slug, :title, :date, :thumbnail_sm, :thumbnail_lg, :video_id, :event_id).includes(:speakers, :event), limit: 12, page: params[:page]&.to_i || 1)
   end
 
   def set_user_favorites
