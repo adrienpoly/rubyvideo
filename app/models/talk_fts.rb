e58a6888ffefcf9b940ef1d277e30e7589b9777a
Adrien Poly <adrienpoly@gmail.com>
2024-10-19 22:52:27 +0200

    prepare env for test

--- app/models/talk_fts.rb
+++ app/models/talk_fts.rb
@@ -2,29 +2,37 @@ class TalkFts < ApplicationRecord
   self.primary_key = "rowid"
   belongs_to :talk, foreign_key: "rowid"
 
-  def self.update_or_create(talk)
-    if exists?(rowid: talk.id)
-      update_fts(talk)
-    else
-      create_fts(talk)
+  class << self
+    def create_index
+      Talk.all.each do |talk|
+        update_or_create(talk)
+      end
     end
-  end
 
-  private
+    def update_or_create(talk)
+      if exists?(rowid: talk.id)
+        update_fts(talk)
+      else
+        create_fts(talk)
+      end
+    end
 
-  def self.update_fts(talk)
-    update_sql = ActiveRecord::Base.sanitize_sql_array([
-      "UPDATE talk_fts SET title = ?, speaker_names = ?, summary = ? WHERE rowid = ?",
-      talk.title, talk.speakers.pluck(:name).join(" "), talk.summary, talk.id
-    ])
-    ActiveRecord::Base.connection.execute(update_sql)
-  end
+    private
 
-  def self.create_fts(talk)
-    insert_sql = ActiveRecord::Base.sanitize_sql_array([
-      "INSERT INTO talk_fts (rowid, title, speaker_names, summary) VALUES (?, ?, ?, ?)",
-      talk.id, talk.title, talk.speakers.pluck(:name).join(" "), talk.summary
-    ])
-    ActiveRecord::Base.connection.execute(insert_sql)
+    def update_fts(talk)
+      update_sql = ActiveRecord::Base.sanitize_sql_array([
+        "UPDATE talk_fts SET title = ?, speaker_names = ?, summary = ? WHERE rowid = ?",
+        talk.title, talk.speakers.pluck(:name).join(" "), talk.summary, talk.id
+      ])
+      ActiveRecord::Base.connection.execute(update_sql)
+    end
+
+    def create_fts(talk)
+      insert_sql = ActiveRecord::Base.sanitize_sql_array([
+        "INSERT INTO talk_fts (rowid, title, speaker_names, summary) VALUES (?, ?, ?, ?)",
+        talk.id, talk.title, talk.speakers.pluck(:name).join(" "), talk.summary
+      ])
+      ActiveRecord::Base.connection.execute(insert_sql)
+    end
   end
 end
--- db/migrate/20241019135118_create_talk_fts.rb
+++ db/migrate/20241019135118_create_talk_fts.rb
@@ -1,5 +1,11 @@
 class CreateTalkFts < ActiveRecord::Migration[7.2]
-  def change
+  def up
     create_virtual_table :talk_fts, :fts5, ["title", "speaker_names", "summary"]
+
+    Talk.all.in_batches.each(&:update_fts_record_later_bulk)
+  end
+
+  def down
+    drop_virtual_table :talk_fts, :fts5, ["title", "speaker_names", "summary"]
   end
 end
