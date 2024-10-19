a858f69c63b586edfd36ee21d7aa90f7dff13b76
Adrien Poly <adrienpoly@gmail.com>
2024-10-19 10:11:20 +0200

    update meilisearch gem

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,6 +1,5 @@
 MeiliSearch::Rails.configuration = {
   meilisearch_url: Rails.env.local? ? "http://localhost:7700" : "http://91.107.208.207:7700", # example: http://localhost:7700
   meilisearch_api_key: Rails.env.local? ? nil : ENV["MEILI_MASTER_KEY"],
-  per_environment: true,
-  timeout: 30
+  per_environment: true
 }
