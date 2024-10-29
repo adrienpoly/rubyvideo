6a2cfbd0fec1c9c8a10253a381f3701c8659cac6
Marco Roth <marco.roth@intergga.ch>
2024-10-29 11:39:39 +0100

    Add new fields to `Youtube::Playlists` class

--- app/clients/youtube/playlists.rb
+++ app/clients/youtube/playlists.rb
@@ -1,18 +1,27 @@
 module Youtube
   class Playlists < Youtube::Client
     DEFAULT_METADATA_PARSER = "Youtube::VideoMetadata"
+
     def all(channel_id:, title_matcher: nil)
       items = all_items("/playlists", query: {channelId: channel_id, part: "snippet,contentDetails"}).map do |metadata|
+        year = metadata.snippet.title.match(/\d{4}/).to_s.presence || DateTime.parse(metadata.snippet.publishedAt).year
+
         OpenStruct.new({
           id: metadata.id,
           title: metadata.snippet.title,
+          location: "Earth",
           description: metadata.snippet.description,
           published_at: DateTime.parse(metadata.snippet.publishedAt).to_date.to_s,
+          start_date: "#{year}-xx-xx",
+          end_date: "#{year}-xx-xx",
           channel_id: metadata.snippet.channelId,
-          year: metadata.snippet.title.match(/\d{4}/).to_s.presence || DateTime.parse(metadata.snippet.publishedAt).year,
+          year: year,
           videos_count: metadata.contentDetails.itemCount,
           metadata_parser: DEFAULT_METADATA_PARSER,
-          slug: metadata.snippet.title.parameterize
+          slug: metadata.snippet.title.parameterize,
+          banner_background: "#FF607F",
+          featured_background: "#000000",
+          featured_color: "#FFFFFF"
         })
       end
       items = items.select { |item| item.title.match?(string_to_regex(title_matcher)) } if title_matcher
