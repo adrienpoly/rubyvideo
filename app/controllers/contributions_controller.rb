924b088a2b90a081e907a11f259a57110f40a4a8
Marco Roth <marco.roth@intergga.ch>
2024-10-31 17:50:41 +0100

    Lint

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -6,12 +6,12 @@ class ContributionsController < ApplicationController
 
     speakers = Speaker.without_github.order(talks_count: :desc)
     @speakers_without_github_count = speakers.count
-    @speakers_without_github = speakers.where.not(id: speaker_ids_with_pending_github_suggestions).limit(11)
+    @speakers_without_github = speakers.where.not(id: speaker_ids_with_pending_github_suggestions).limit(1000)
 
     speakers_with_speakerdeck = Speaker.where.not(speakerdeck: "")
 
     talks = Talk.joins(:speakers).where(slides_url: nil).where(speakers: {id: speakers_with_speakerdeck}).order(date: :desc)
     @talks_without_slides_count = talks.count
-    @talks_without_slides = talks.limit(11)
+    @talks_without_slides = talks.limit(1000)
   end
 end
