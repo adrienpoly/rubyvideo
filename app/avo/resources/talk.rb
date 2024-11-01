c03af9c4682193ec56c8b1d8314b620a8e54564d
Adrien Poly <adrienpoly@gmail.com>
2024-11-01 22:47:06 +0100

    add external_player to admin

--- app/avo/resources/talk.rb
+++ app/avo/resources/talk.rb
@@ -43,6 +43,7 @@ class Avo::Resources::Talk < Avo::BaseResource
     field :year, as: :number, hide_on: :index
     field :video_id, as: :text, hide_on: :index
     field :video_provider, as: :text, hide_on: :index
+    field :external_player, as: :boolean, hide_on: :index
     field :thumbnail_xs, as: :external_image, hide_on: :index
     field :thumbnail_sm, as: :external_image, hide_on: :index
     field :thumbnail_md, as: :external_image, hide_on: :index
