69ed6aa805b54886235de1f6ee6d5ce5c285f638
Marco Roth <marco.roth@intergga.ch>
2024-11-01 22:11:49 +0100

    Improve messaging and UI for `external_player` (#349)

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -14,13 +14,17 @@ module IconHelper
     inline_svg_tag "icons/heroicons/#{variant}/#{icon_name.to_s.tr("_", "-")}.svg", class: classes, **options.except(:class)
   end
 
-  def fontawesome(icon_name, size: :md, type: :regular, style: :solid, **options)
+  def fontawesome(icon_name, size: :md, type: nil, style: :solid, **options)
     classes = class_names(SIZE_CLASSES[size], options[:class])
-    inline_svg_tag "icons/fontawesome/#{icon_name}-#{type}-#{style}.svg", class: classes, **options.except(:class)
+    inline_svg_tag "icons/fontawesome/#{[icon_name, type, style].compact.join("-")}.svg", class: classes, **options.except(:class)
   end
 
   def fab(icon_name, **options)
     fontawesome(icon_name, type: :brands, **options.except(:type))
+  end
+
+  def fa(icon_name, **options)
+    fontawesome(icon_name, **options.except(:type))
   end
 
   def icon(icon_name, size: :md, **options)
