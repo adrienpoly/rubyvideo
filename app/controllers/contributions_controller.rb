72db98ec7dd73e095f101eb7fea1b1c15923a9c2
Marco Roth <marco.roth@intergga.ch>
2024-11-01 12:42:55 +0100

    Lint

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -19,20 +19,18 @@ class ContributionsController < ApplicationController
     @events_without_dates = Static::Playlist.where(start_date: nil).group_by(&:__file_path)
     @events_without_dates_count = @events_without_dates.flat_map(&:last).count
 
-
     # Review Talk Dates
 
-    @dates_by_event_name = (
+    @dates_by_event_name =
       Static::Playlist.all.where.not(start_date: nil).pluck(:title, :start_date, :end_date).select { |_, start_date| start_date.present? }.map { |name, start_date, end_date| [name, Date.parse(start_date)..Date.parse(end_date)] }.union(
         Static::Playlist.all.where(start_date: nil).pluck(:title, :year).map { |title, year| [title, Date.parse("#{year}-01-01").all_year] }
       )
-    ).to_h
+        .to_h
 
     talks_by_event_name = Talk.preload(:event).to_a.select { |talk| talk.event.name.in?(@dates_by_event_name.keys) }.group_by(&:event)
 
     @out_of_bound_talks = talks_by_event_name.map { |event, talks| [event, talks.reject { |talk| @dates_by_event_name[event.name].cover?(talk.date) }] }
     @out_of_bound_talks_count = @out_of_bound_talks.map(&:last).flatten.count
-
 
     # Missing events
 
