3bba613bbedb94792e7d1bb31ebee0fd8277dd57
Nicolás Galdámez <nicolas.galdamez@unagisoftware.com>
2024-11-05 11:30:05 -0300

    Standard as default

--- db/migrate/20241031112333_add_kind_to_talk.rb
+++ db/migrate/20241031112333_add_kind_to_talk.rb
@@ -1,9 +1,5 @@
 class AddKindToTalk < ActiveRecord::Migration[8.0]
   def change
-    add_column :talks, :kind, :string, null: true
-
-    Talk.update_all kind: :standard
-
-    change_column_null :talks, :kind, false
+    add_column :talks, :kind, :string, null: true, default: 'standard'
   end
 end
