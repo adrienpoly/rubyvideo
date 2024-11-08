775daa0e75f7b9696cc936bb4cc95924a3eec26f
Marco Roth <marco.roth@intergga.ch>
2024-11-08 12:35:01 +0100

    Upgrade Rails to `8.0.0`

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Change to :null_store to avoid any caching.
+  # config.cache_store = :memory_store
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
