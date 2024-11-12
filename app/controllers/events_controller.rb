976556013acbd7547f1d3392af425f03f4b5dccc
Adrien Poly <adrienpoly@gmail.com>
2024-11-12 21:36:21 +0100

    make events and speakers searchable with basic LIKE

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,7 @@ class EventsController < ApplicationController
   def index
     @events = Event.canonical.includes(:organisation).order("events.name ASC")
     @events = @events.where("lower(events.name) LIKE ?", "#{params[:letter].downcase}%") if params[:letter].present?
+    @events = @events.ft_search(params[:s]) if params[:s].present?
   end
 
   # GET /events/1
