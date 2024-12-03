bd7bda9c0642b4107392b5f5a6b8eca608ceb77d
Marco Roth <marco.roth@intergga.ch>
2024-12-03 01:03:00 +0100

    Extrac _header_content to being able to broadcast changes

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -161,7 +161,7 @@ class Speaker < ApplicationRecord
   end
 
   def broadcast_header
-    broadcast_update_to self, target: dom_id(self, :header), partial: "speakers/header", locals: {speaker: self}
+    broadcast_update_to self, target: dom_id(self, :header_content), partial: "speakers/header_content", locals: {speaker: self}
   end
 
   def valid_website_url
