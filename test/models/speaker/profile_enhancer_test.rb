d5dad7497b11016be53a705ecc17d2696ed5b93e
Kasper Timm Hansen <hey@kaspth.com>
2024-12-06 18:13:30 +0100

    Also rename the test

--- test/models/speaker/profile_enhancer_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Speaker::ProfileEnhancerTest < ActiveSupport::TestCase
-  setup do
-    # @speaker = speakers(:TODO_fixture_name)
-    # @profile_enhancer = @speaker.profile_enhancer
-  end
-end
---
+++ test/models/speaker/profiles_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class Speaker::ProfilesTest < ActiveSupport::TestCase
+  setup do
+    # @speaker = speakers(:TODO_fixture_name)
+    # @profiles = @speaker.profiles
+  end
+end
