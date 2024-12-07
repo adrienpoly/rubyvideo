4658e1359edd269c0d22017a98ef7b11c90252cc
Marco Roth <marco.roth@intergga.ch>
2024-12-07 19:47:53 +0100

    Fix `Speakers::EnhanceControllerTest`

--- test/controllers/speakers/enhance_controller_test.rb
+++ test/controllers/speakers/enhance_controller_test.rb
@@ -6,7 +6,17 @@ class Speakers::EnhanceControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "#patch" do
-    patch speakers_enhance_url(speakers(:one), {format: :turbo_stream})
-    assert_response :success
+    speaker = speakers(:one)
+
+    assert_nil speaker.github_metadata.dig("profile", "login")
+
+    VCR.use_cassette("speakers/enhance_controller_test/patch") do
+      patch speakers_enhance_url(speaker, {format: :turbo_stream})
+      assert_response :success
+    end
+
+    speaker.reload
+
+    assert_equal "obie", speaker.github_metadata.dig("profile", "login")
   end
 end
