f224af19069300cfd1c0f560b1fff41f2aa21e99
Adrien Poly <adrienpoly@gmail.com>
2024-12-19 07:40:09 +0100

    lazy load some contribution routes

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,24 +1,16 @@
 class ContributionsController < ApplicationController
+  include Turbo::ForceFrameResponse
+  force_frame_response only: %i[show]
   skip_before_action :authenticate_user!, only: %i[index]
 
+  STEPS = %i[speakers_without_github talks_without_slides events_without_videos].freeze
+
   def index
-    speaker_ids_with_pending_github_suggestions = Suggestion.pending.where("json_extract(content, '$.github') IS NOT NULL").where(suggestable_type: "Speaker").pluck(:suggestable_id)
-    @speakers_without_github = Speaker.canonical.without_github.order(talks_count: :desc).where.not(id: speaker_ids_with_pending_github_suggestions)
-    @speakers_without_github_count = @speakers_without_github.count
-
-    speakers_with_speakerdeck = Speaker.where.not(speakerdeck: "")
-    @talks_without_slides = Talk.preload(:speakers).joins(:speakers).where(slides_url: nil).where(speakers: {id: speakers_with_speakerdeck}).order(date: :desc)
-    @talks_without_slides_count = @talks_without_slides.count
-
-    @events_without_videos = Event.includes(:organisation).left_joins(:talks).where(talks_count: 0).group_by(&:organisation)
-    @events_without_videos_count = @events_without_videos.flat_map(&:last).count
-
     @events_without_location = Static::Playlist.where(location: nil).group_by(&:__file_path)
     @events_without_location_count = @events_without_location.flat_map(&:last).count
 
     @events_without_dates = Static::Playlist.where(start_date: nil).group_by(&:__file_path)
     @events_without_dates_count = @events_without_dates.flat_map(&:last).count
-
     # Review Talk Dates
 
     events_with_start_date = Static::Playlist.all.pluck(:title, :start_date, :end_date).select { |_, start_date| start_date.present? }
@@ -52,7 +44,11 @@ class ContributionsController < ApplicationController
     # Missing events
 
     conference_names = Event.all.pluck(:name)
-    @upstream_conferences = RubyConferences::Client.new.conferences_cached.reverse
+    @upstream_conferences = begin
+      RubyConferences::Client.new.conferences_cached.reverse
+    rescue
+      []
+    end
     @pending_conferences = @upstream_conferences.reject { |conference| conference["name"].in?(conference_names) }
 
     @with_video_link, @without_video_link = @pending_conferences.partition { |conference| conference["video_link"].present? }
@@ -69,5 +65,48 @@ class ContributionsController < ApplicationController
     # Conferences with missing schedules
     @conferences_with_missing_schedule = Event.joins(:organisation).where(organisation: {kind: :conference}).reject { |event| event.schedule.exist? }.group_by(&:organisation)
     @conferences_with_missing_schedule_count = @conferences_with_missing_schedule.flat_map(&:last).count
+  end
+
+  def show
+    @step = params[:step].to_sym.presence_in(STEPS)
+
+    if @step
+      send(@step)
+    else
+      raise StandardError, "missing step"
+    end
+  end
+
+  def speakers_without_github
+    speaker_ids_with_pending_github_suggestions = Suggestion.pending.where("json_extract(content, '$.github') IS NOT NULL").where(suggestable_type: "Speaker").pluck(:suggestable_id)
+    @speakers_without_github = Speaker.canonical.without_github.order(talks_count: :desc).where.not(id: speaker_ids_with_pending_github_suggestions)
+    @speakers_without_github_count = @speakers_without_github.count
+  end
+
+  def talks_without_slides
+    speakers_with_speakerdeck = Speaker.where.not(speakerdeck: "")
+    @talks_without_slides = Talk.preload(:speakers).joins(:speakers).where(slides_url: nil).where(speakers: {id: speakers_with_speakerdeck}).order(date: :desc)
+    @talks_without_slides_count = @talks_without_slides.count
+  end
+
+  def events_without_videos
+    @events_without_videos = Event.includes(:organisation).left_joins(:talks).where(talks_count: 0).group_by(&:organisation)
+    @events_without_videos_count = @events_without_videos.flat_map(&:last).count
+
+    @events_without_location = Static::Playlist.where(location: nil).group_by(&:__file_path)
+    @events_without_location_count = @events_without_location.flat_map(&:last).count
+
+    @events_without_dates = Static::Playlist.where(start_date: nil).group_by(&:__file_path)
+    @events_without_dates_count = @events_without_dates.flat_map(&:last).count
+  end
+
+  def events_without_location
+    @events_without_location = Static::Playlist.where(location: nil).group_by(&:__file_path)
+    @events_without_location_count = @events_without_location.flat_map(&:last).count
+  end
+
+  def events_without_dates
+    @events_without_dates = Static::Playlist.where(start_date: nil).group_by(&:__file_path)
+    @events_without_dates_count = @events_without_dates.flat_map(&:last).count
   end
 end
