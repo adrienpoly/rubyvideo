ded2e39f960c08229dde4de844d0f6b8459a85c5
Nicolás Galdámez <nicolas.galdamez@unagisoftware.com>
2024-12-30 11:32:17 -0300

    Adds duration to talks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_03_001515) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_27_192232) do
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
     t.integer "user_id"
@@ -204,13 +204,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_03_001515) do
     t.string "language", default: "en", null: false
     t.string "slides_url"
     t.boolean "summarized_using_ai", default: true, null: false
+    t.string "kind", default: "talk", null: false
     t.boolean "external_player", default: false, null: false
     t.string "external_player_url", default: "", null: false
-    t.string "kind", default: "talk", null: false
     t.integer "parent_talk_id"
     t.boolean "meta_talk", default: false, null: false
     t.integer "start_seconds"
     t.integer "end_seconds"
+    t.integer "duration_in_seconds"
     t.index ["date"], name: "index_talks_on_date"
     t.index ["event_id"], name: "index_talks_on_event_id"
     t.index ["kind"], name: "index_talks_on_kind"
