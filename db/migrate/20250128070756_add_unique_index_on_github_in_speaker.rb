c1e2199bf8282960964ba7d56584ff8668c190c8
Adrien Poly <adrienpoly@gmail.com>
2025-01-28 11:16:00 +0100

    wip

--- db/migrate/20250128070756_add_unique_index_on_github_in_speaker.rb
+++ db/migrate/20250128070756_add_unique_index_on_github_in_speaker.rb
@@ -1,8 +1,29 @@
 class AddUniqueIndexOnGitHubInSpeaker < ActiveRecord::Migration[8.0]
   def change
-    # remove all github handles for speakers having a canonical speaker
-    Speaker.where.not(canonical_id: nil).update_all(github: "")
+    # remove duplicate with no talks
+    Speaker.group(:github).having("count(*) > 1").pluck(:github).each do |github|
+      Speaker.where(github: github, talks_count: 0).update_all(github: "")
+    end
+
+    # fix one by one
+    Speaker.find_by(name: "Andrew").assign_canonical_speaker!(canonical_speaker: Speaker.find_by(name: "Andrew Nesbitt"))
     puts Speaker.group(:github).having("count(*) > 1").pluck(:github)
-    add_index :speakers, :github, unique: true
+    # TODO
+    # CoralineAda
+    # JEG2
+    # egiurleo
+    # eileencodes
+    # gus4no
+    # hasumikin
+    # headius
+    # hogelog
+    # hsbt
+    # jonatas
+    # kou
+    # mrzasa
+    # okuramasafumi
+    # olivierlacan
+    # wndxlori
+    add_index :speakers, :github, unique: true, where: "github IS NOT NULL AND github != ''"
   end
 end
