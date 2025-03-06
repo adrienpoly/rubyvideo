55e615ee0f95ee4a55f4899174cd91e8256f1569
Adrien Poly <adrienpoly@gmail.com>
2025-03-06 14:35:15 +0100

    add yml formater rake task

--- lib/tasks/yml.rake
+++ lib/tasks/yml.rake
@@ -4,27 +4,16 @@ namespace :yml do
   desc "normalize the yml file"
   task normalize: :environment do
     Dir.glob("data/**/*/videos.yml").each do |videos_yml_path|
-      YmlFormatter.normalize_commented_yaml_file(videos_yml_path)
-
       # first we normalize the comments
       yaml_content = File.read(videos_yml_path)
       reformatted_content = YmlFormatter.normalize_comment_lines(yaml_content)
 
       # then we normalize the content
-      # reformatted_content = YmlFormatter.normalize_content_lines(reformatted_content)
+      reformatted_content = YmlFormatter.normalize_commented_yaml_file(reformatted_content)
       File.write(videos_yml_path, reformatted_content)
     end
 
     # then we ensure the style is correct with prettier
     system("yarn format:yml")
-  end
-
-  desc "normalize comments empty lines"
-  task normalize_comments_empty_lines: :environment do
-    Dir.glob("data/**/*/videos.yml").each do |videos_yml_path|
-      lines = File.readlines(videos_yml_path, chomp: true)
-      reformatted_content = YmlFormatter.normalize_comment_lines(lines)
-      File.write(videos_yml_path, reformatted_content + "\n")
-    end
   end
 end
--- lib/yml_formatter.rb
+++ lib/yml_formatter.rb
@@ -19,6 +19,42 @@ module YmlFormatter
     Psych::Comments.emit_yaml(ast)
   end
 
-  def normalize_commented_yaml_file(file_path)
+  def normalize_commented_yaml_file(yaml_content)
+    ast = Psych::Comments.parse_stream(yaml_content)
+    document = ast.children.first
+
+    talks_sequence = document.root.children
+
+    talks_sequence.each do |talk|
+      next unless talk.is_a?(Psych::Nodes::Mapping)
+
+      # Iterate over the children of the mapping node in pairs (key, value)
+      talk.children.each_slice(2).with_index do |(key_node, value_node), index|
+        next unless value_node.is_a?(Psych::Nodes::Scalar)
+        next if value_node.value.blank?
+        # normalize the description field
+        #
+        # expected output ->
+        # description: |-
+        #   the multi line description
+        #   of the talk
+        #
+        if key_node.value == "description" || key_node.value == "description_2"
+          value_node.plain = false
+          value_node.quoted = true
+          value_node.style = 4 # multiline |- style output
+          value_node.value = value_node.value.gsub(" \n", "\n")
+
+          # remove only trailing newlines at the end of the entire text
+          # while preserving internal line breaks
+          value_node.value = value_node.value.sub(/\n+\z/, "")
+          value_node.start_line = key_node.start_line + 1
+          value_node.start_column = key_node.start_column + 2
+          value_node.end_line = 0
+          value_node.end_column = 0
+        end
+      end
+    end
+    Psych::Comments.emit_yaml(ast)
   end
 end
