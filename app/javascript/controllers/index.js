4ea0b3573242537bcc0628770f6cca4ccb57418a
Marco Roth <marco.roth@intergga.ch>
2025-03-30 02:10:54 -0300

    Titles

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -57,6 +57,3 @@ application.register("video-player", VideoPlayerController)
 
 import ButtonController from "./bridge/button_controller"
 application.register("bridge--button", ButtonController)
-
-import LargeTitleController from "./bridge/large_title_controller"
-application.register("bridge--large-title", LargeTitleController)
